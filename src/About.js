f5f334066915bbc340266254cdd1c02deccc92c8
sjpaige <sebastianpaige@gmail.com>
2021-07-08 15:27:30 -0700

    Added Images to about pages

--- src/About.js
+++ src/About.js
@@ -1,17 +1,17 @@
-import React, { useContext } from 'react';
-import { Typography, makeStyles, Grid, Container,} from '@material-ui/core';
-import organizedImage from './static/about/organized.jpg'
-import { FirebaseContext } from './components/Firebase';
-
+import React from 'react';
+import { Typography, makeStyles, Grid, Container, Box} from '@material-ui/core';
+import resume from './static/about/resume.jpg';
+import devices from './static/about/devices.jpg';
+import metrics from './static/about/metrics.jpg';
+import ImageCard from './ImageCard.js';
 
 const useStyles = makeStyles((theme) => ({
     root:{
         marginTop: theme.spacing(3),
+       
     },
-    organized:{
-        maxWidth: '300px',
-        backgroundImage: `url(${organizedImage})`,
-        borderRadius: '5px',
+    box: {
+        justifyContent: 'center',
     }
 }))
 
@@ -20,22 +20,25 @@ export default function About(){
     const classes = useStyles();
     return(
         <Container maxWidth="lg" className={classes.root}>
-           <img className={classes.organized} src={organizedImage} alt="organized using computer."/>
-            <Grid container>
-                <Grid item lg={4} md={6} xs={12}>
+           <Grid container spacing={3}>
+                <Grid item lg={6} md={12} xs={12}><ImageCard image={resume}/></Grid>
+                <Grid item lg={6} md={12} xs={12}>
                     <Typography variant="h4" align="center"> Stay Organized</Typography>
                     <Typography variant="body1" align="center"> 
                         Manage your job search using the Job Ghost tool. You can track where you are
                         in the process and fill out the requirements for the position.
                     </Typography>   
                 </Grid>
-                <Grid item lg={4} md={6} xs={12}>
+                
+                <Grid item lg={6} md={6} xs={12}>
                     <Typography variant="h4" align="center">Available Anywhere</Typography>
                     <Typography align="center"> 
                         Job Ghost is available as a web tool on both mobile and pc web platforms.
                     </Typography>
                 </Grid>
-                <Grid item lg={4} md={6} xs={12}>
+                <Grid item lg={6} md={12} xs={12}><ImageCard image={devices}/></Grid>
+                <Grid item lg={6} md={12} xs={12}><ImageCard image={metrics}/></Grid>
+                <Grid item lg={6} md={6} xs={12}>
                     <Typography variant="h4" align="center">Keep Track</Typography>
                     <Typography align="center"> 
                         Track once you have applied, when you interview, and finally if you get an offer.
--- src/ImageCard.js
+++ src/ImageCard.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core';
-import organizedImage from './static/about/organized.jpg'
 
 
 const useStyles = makeStyles((theme) => ({
 
     organized:{
-        maxWidth: '300px',
-        backgroundImage: `url(${organizedImage})`,
+        maxWidth: '100%',
         borderRadius: '5px',
     }
 }))
 
-function ImageCard() {
+export default function ImageCard(props) {
+
+    const image = props.image;
     
     const classes = useStyles();
+
     return(
-        <img className={classes.organized} src={organizedImage} alt="organized using computer."/>
+        <img className={classes.organized} src={image} alt="organized using computer."/>
     )
 }
