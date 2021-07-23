be106a5462c59287fbfb26239f60c9238cfd53e9
sjpaige <sebastianpaige@gmail.com>
2021-07-23 14:01:55 -0700

    Added authentication using github login

--- src/SignIn.js
+++ src/SignIn.js
@@ -17,7 +17,8 @@ const useStyles = makeStyles((theme) => ({
     paddingBottom: theme.spacing(3),
   },
   signInButton:{
-    margin: theme.spacing(2),
+    margin: theme.spacing(1),
+    width: '250px',
   },
   title:{
     marginBottom: theme.spacing(5),
@@ -28,7 +29,9 @@ export default function SignIn() {
 
     const firebase = useContext(FirebaseContext);
 
-    const signIn = () => firebase.signInWithGoogle();
+    const signInGoogle = () => firebase.signInWithGoogle();
+    const signInWithGithub = () => firebase.signInWithGoogle();
+
     
     const classes = useStyles();
 
@@ -38,7 +41,9 @@ export default function SignIn() {
             <GhostLogo height={150} width={150}/>
             <Typography variant="h2" className={classes.title} >Job Ghost</Typography>
             <Typography component="h1" variant="h4" color="primary">Sign In</Typography>
-            <Button className={classes.signInButton} variant="contained" color="secondary"onClick={signIn}>Sign in with Google</Button> 
+            <Button className={classes.signInButton} variant="contained" color="secondary" onClick={signInGoogle}>Sign in with Google</Button> 
+            <Button className={classes.signInButton} variant="contained" color="secondary" onClick={signInWithGithub}>Sign in with Github</Button> 
+
           </Paper>
         </Container>
     ) 
